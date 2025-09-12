a42bd3e529ced933a70a4a85f9b59be867150056
angelcruz07 <angelcruzsanchez126@gmail.com>
2025-09-11 21:09:33 -0600

    feat: add config better auth

--- src/lib/auth/auth-client.ts
+++ src/lib/auth/auth-client.ts
@@ -1,8 +1,5 @@
-import { auth } from "./auth";
-import type { APIRoute } from "astro";
+import { createAuthClient } from "better-auth/client";
 
-export const ALL: APIRoute = async (ctx) => {
-  // If you want to use rate limiting, make sure to set the 'x-forwarded-for' header to the request headers from the context
-  // ctx.request.headers.set("x-forwarded-for", ctx.clientAddress);
-  return auth.handler(ctx.request);
-};
+const authClient = createAuthClient(); 
+
+export const { signIn, signOut } = authClient;
