310a2a1de87c7336aa8c556d9ccdcce1451f142e
LizandroBackEnd <codeversebackend@gmail.com>
2025-09-14 18:30:37 -0600

    fix(logical): Refactor code and logical

--- src/actions/post/createUpdatePost.action.ts
+++ src/actions/post/createUpdatePost.action.ts
@@ -8,15 +8,13 @@ export const createUpdatePost = defineAction({
         id: z.string().optional(),
         title: z.string().min(3).max(50),
         content: z.string().min(10),
-        likes: z.number(),
-        images: z.array(z.string().url()).optional(),
+        image: z.array(z.string().url()).optional(),
         tags: z.array(z.string().min(3).max(20)),
         slug: z.string().min(5).max(30),
         author: z.number(),
         categories: z.array(z.string().min(3).max(50)),
-        comments: z.array(z.string().min(10).max(250))
     }),
-    handler: async({id, title, content, likes, images, tags, slug, author, categories, comments}) => {
+    handler: async ({ id, title, content, image, tags, slug, author, categories }) => {
         try {
             if (id) {
                 const updatedPost = await prisma.post.update({
@@ -24,47 +22,39 @@ export const createUpdatePost = defineAction({
                     data: {
                         title,
                         content,
-                        likes,
-                        images,
+                        image,
                         tags,
                         slug,
                         author: { connect: { id: author } },
                         categories: {
                             connect: categories.map(name => ({ name }))
-                        },
-                        comments: {
-                            create: comments.map(content => ({ content }))
                         }
                     }
                 });
                 return updatedPost;
-            } else {
-                const newPost = await prisma.post.create({
-                    data: {
-                        title,
-                        content,
-                        likes,
-                        images,
-                        tags,
-                        slug,
-                        author: { connect: { id: author } },
-                        categories: {
-                            connect: categories.map(name => ({ name }))
-                        },
-                        comments: {
-                            create: comments.map(content => ({ content }))
-                        }
-                    }
-                });
-                return newPost;
             }
+            const newPost = await prisma.post.create({
+                data: {
+                    title,
+                    content,
+                    image,
+                    tags,
+                    slug,
+                    author: { connect: { id: author } },
+                    categories: {
+                        connect: categories.map(name => ({ name }))
+                    }
+                }
+            });
+            return newPost;
+
         } catch (e) {
-            if (id) { 
+            if (id) {
                 throw new ActionError({
                     code: "BAD_REQUEST",
                     message: "Error to update the post" + (e instanceof Error ? e.message : String(e))
                 });
-            } else { 
+            } else {
                 throw new ActionError({
                     code: "BAD_REQUEST",
                     message: "Error to create the post" + (e instanceof Error ? e.message : String(e))
