11f223312f613e44a5241abb6552b489a2dfdc27
LizandroBackEnd <codeversebackend@gmail.com>
2025-09-19 19:10:06 -0600

    fix(action): Refactor action get all posts

--- src/actions/post/getPosts.action.ts
+++ src/actions/post/getPosts.action.ts
@@ -2,15 +2,11 @@ import { ActionError, defineAction } from "astro:actions";
 import prisma from "@lib/prisma";
 
 export  const getPosts = defineAction({ 
-    accept: 'form',
     handler: async () => { 
         try { 
             const posts = await prisma.post.findMany({ 
                 include: { 
                     author: true, 
-                    title: true,
-                    content: true,
-                    likes: true,
                     categories: true,
                     comments: true
                 }
