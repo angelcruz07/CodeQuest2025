c7ee4a4e52fd2da4b8a1dacbbe92d7e252f583ea
angelcruz07 <angelcruzsanchez126@gmail.com>
2025-09-20 11:35:29 -0600

    feat(pagination): Implement pagination component and update post fetching actions

--- src/actions/post/index.ts
+++ src/actions/post/index.ts
@@ -2,12 +2,12 @@ import { createUpdatePost } from "./createUpdatePost.action";
 import { deletePost } from "./deletePost.action";
 import { getPostBySlug } from "./getPostBySlug.action";
 import { getPosts } from "./getPosts.action";
-import getPostByPage from "./getPostByPage.action";
+import { getPostsByPage } from "./getPostsByPage.action";
 
 export const post = {
   createUpdatePost,
   deletePost,
   getPostBySlug,
-  getPostByPage,
+  getPostsByPage,
   getPosts,
 };
