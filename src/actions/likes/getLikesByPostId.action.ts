e88bab8ea7cf7a1eb5f249634b16b756f80d8c11
LizandroBackEnd <codeversebackend@gmail.com>
2025-09-22 16:45:42 -0600

    feat(action): Added action for found likes by post

---
+++ src/actions/likes/getLikesByPostId.action.ts
@@ -0,0 +1,24 @@
+import { ActionError, defineAction } from "astro:actions";
+import { z } from "astro:schema";
+import prisma from "@lib/prisma";
+
+export const getLikesByPostId = defineAction({ 
+    accept: "form", 
+    input: z.object({
+        postId: z.string(),
+    }),
+    handler: async ({ postId }) => { 
+        try { 
+            const likes = await prisma.like.findMany({
+                where: { postId },
+            }); 
+            return { count: likes.length, likes };
+        } catch (e) { 
+            throw new ActionError({ 
+                code: "BAD_REQUEST",
+                message: "Error fetching likes for post." + (e instanceof Error ? e.message : String(e)),
+            })
+        }
+        
+    }
+})
