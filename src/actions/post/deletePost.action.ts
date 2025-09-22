94fedbabee58640d988f773bdea6dbc42d87eec5
angelcruz07 <angelcruzsanchez126@gmail.com>
2025-09-21 22:09:09 -0600

    refactor(post): streamline deletePost action and improve error handling

--- src/actions/post/deletePost.action.ts
+++ src/actions/post/deletePost.action.ts
@@ -4,45 +4,45 @@ import { ActionError, defineAction } from "astro:actions";
 import { deleteImage } from "@lib/cloudinary/deleteImage";
 
 export const deletePost = defineAction({
-    accept: 'form',
-    input: z.object({
-        id: z.string()
-    }),
-    handler: async ({ id }) => {
-        try {
-            const post = await prisma.post.findUnique({
-                where: { id }
-            });
-            
-            if (!post) {
-                throw new ActionError({
-                    code: "NOT_FOUND",
-                    message: "Post not found"
-                });
-            }
+  input: z.object({
+    id: z.string(),
+  }),
+  handler: async ({ id }) => {
+    try {
+      const post = await prisma.post.findUnique({
+        where: { id },
+      });
 
-            //Elimina la foto de cloudinary
-            if (post.image) {
-                if (Array.isArray(post.image)) {
-                    for (const imgUrl of post.image) {
-                        if (imgUrl) await deleteImage(imgUrl);
-                    }
-                } else if (typeof post.image === 'string') {
-                    await deleteImage(post.image);
-                }
-            }
+      if (!post) {
+        throw new ActionError({
+          code: "NOT_FOUND",
+          message: "Post not found",
+        });
+      }
 
-            //Elimina la foto de la base de datos
-            const deletedPost = await prisma.post.delete({
-                where: { id: id }
-
-            });
-            return deletedPost;
-        } catch (e) {
-            throw new ActionError({
-                code: "BAD_REQUEST",
-                message: "Error to delete the post" + (e instanceof Error ? e.message : String(e))
-            });
+      //Elimina la foto de cloudinary
+      if (post.image) {
+        if (Array.isArray(post.image)) {
+          for (const imgUrl of post.image) {
+            if (imgUrl) await deleteImage(imgUrl);
+          }
+        } else if (typeof post.image === "string") {
+          await deleteImage(post.image);
         }
+      }
+
+      //Elimina la foto de la base de datos
+      const deletedPost = await prisma.post.delete({
+        where: { id: id },
+      });
+      return deletedPost;
+    } catch (e) {
+      throw new ActionError({
+        code: "BAD_REQUEST",
+        message:
+          "Error to delete the post" +
+          (e instanceof Error ? e.message : String(e)),
+      });
     }
+  },
 });
